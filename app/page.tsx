3af0797adad3f3ef7f9f7650ed7e4c23b7356b8b
KenzG <kenjitenzai489@gmail.com>
2026-01-31 21:19:49 +0700

    main

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import { Gallery } from "@/components/gallery";
+import Bio from "@/components/bio";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black">
-      <Gallery />
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-zinc-50 dark:bg-black">
+      <section className="w-full max-w-2xl mb-12">
+        <h2 className="text-2xl font-bold text-center mb-4">My Gallery</h2>
+        <Gallery />
+      </section>
+       <hr className="w-full max-w-md border-zinc-300 mb-12" />
+
+      <section className="w-full max-w-2xl">
+        <h2 className="text-2xl font-hold text-center mb-6">My Bio</h2>
+        <Bio />
+      </section>
+    </main>
   );
 }
---
+++ src/components/bio.tsx
@@ -0,0 +1,17 @@
+export default function Bio() {
+    return (
+        <>
+        <div>
+            <h1>Welcome to the Website</h1>    
+        </div>
+            <p className="summary">
+                You can read my strangled mind here
+                <br>
+                </br>
+                There is a <i>picture</i> of 
+            {" "}
+            <b>someone!</b>
+        </p>
+        </>
+    );
+}

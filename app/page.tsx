b47d250684d4ccc472b9d76dbc0e54fbd60e0f37
KenzG <kenjitenzai489@gmail.com>
2026-02-27 09:05:19 +0700

    Changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import { Gallery } from "@/components/gallery";
 import Bio from "@/components/bio";
 import TodoList from "@/components/todolist";
+import Profile from "@/components/avatar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-zinc-50 dark:bg-black">
+      <section className="w-full max-w-2xl mb-12">
+        <h2 className="text-2xl font-bold text-center mb-4">Avatar</h2>
+        <Profile />
+      </section>
+      <hr className="w-full max-w-md border-zinc-300 mb-12"/>
+      
       <section className="w-full max-w-2xl mb-12">
         <h2 className="text-2xl font-bold text-center mb-4">My Gallery</h2>
         <Gallery />
--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,17 +1,46 @@
-function Avatar() {
+import { getImageUrl } from '@/utils/utils';
+
+type Person = {
+  name: string;
+  imageId: string;
+};
+
+function Avatar({ person, size }: { person: Person; size: number }) {
     return (
         <img
             className="avatar"
-            src="https://imgur.com/undefined"
-            alt="Jack Cooper"
-            width={100}
-            height={100}
+            src={getImageUrl(person)}
+            alt={person.name}
+            width={size}
+            height={size}
         />
     );
 }
 
-export default fucntion Profile() {
+export default function Profile() {
     return (
-        <Avatar />
+        <div className="columns-3">
+        <Avatar 
+            size={100}
+            person={{
+                name: 'Jack Cooper',
+                imageId: 'XmhgzOX'
+            }}
+        />
+        <Avatar
+            size={100}
+            person={{
+                name: 'Kuben Blisk',
+                imageId: 'HRmGv0a'
+            }}
+        />
+        <Avatar 
+            size={100}
+            person={{
+                name: 'VIPER',
+                imageId: 'k3e2iXb'
+            }}
+        />
+        </div>
     );
 }
